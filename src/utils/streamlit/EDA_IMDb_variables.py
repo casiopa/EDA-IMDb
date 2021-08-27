eba63f6f25cc24cc306207ba5a2ae4cae3d149cc
Ana Blanco <anablancodelgado@gmail.com>
2021-08-27 16:51:23 +0200

    Update text intro sreamlit
    Updated the

--- src/utils/streamlit/EDA_IMDb_variables.py
+++ src/utils/streamlit/EDA_IMDb_variables.py
@@ -6,22 +6,26 @@ intro = '''
 ### Análisis exploratorio de datos | Películas 2014 a 2019
 Con la intención de analizar un dataset representativo de todo tipo de películas he acudido al portal IMDb, el más completo a nivel internacional. Y he extraído los datos por dos vías: 
 
-##### 1. Tablas relacionales descargadas de IMBd
+#### 1. Tablas relacionales descargadas de IMBd
 IMDb ofrece, de manera gratuita, una serie de archivos `.csv` que se corresponden con parte la información de su base de datos. De estas tablas he conseguido información como identificador de IMDb, título en español y en su versión original, duración, géneros, rating IMDb, año.
 
-##### 2. *Web scrapping* portal IMBd
+#### 2. *Web scrapping* portal IMBd
 Como los archivos proporcionados por IMDb no contienen información económica de las películas he recogido esta información del propio portal IMDb mediante *web scrapping*. También era importante el dato de valoración de la crítica, el Metascore que sin ser un dato propio de IMBd sí que se puede visualizar en el portal. Además he recogido información que podría ser relevante más adelante como directores, guionistas, actores y países. Debido a la ingente cantidad de información, he tenido que utilizar parallel para recoger información simultáneamente de varias páginas simultáneamente, una página por núcleo del procesador de mi portátil. La herramienta principal en esta estapa fue Selenium.
 > **117.482 páginas escrapeadas** (todas las películas de 2014 a 2020)
 
-##### 3. *Data mining* y *merge* de las tablas
+#### 3. *Variables* de estudio
+Una vez que tenemos diponible todos los datos seleccionamos nuestras principales variables para el estudio.
+Nos encontramos con dos tipos de variables: Por un lado tenemos las variables que hacen una valoración de las películas - Rating IMDb de usuarios, Metascore- y por otra parte tenemos variabes tipo económico (Presupuesto, Recaudación, Beneficio y ROI).
+
+#### 4. *Data mining* y *merge* de las tablas
 El siguiente paso fue limpiar las tablas ya que había muchos registros *fake*, tanto en las tablas descargadas de IMDb como en el portal.
 En esta fase fue necesario convertir la información escrapeada del portal ya que todo era texto. En el caso del presupuesto y la recaudación también fue necesario separar la información de la moneda y la cantidad. La moneda tuvo que ser trasladada al código ISO correspondiente para poder aisgnarle la tasa de cambio correspondiente a la moneda y el año. Finalmente normalicé los valores en dólares para todas las películas.
 Finalmente, teniendo la información de presupuesto y recaudación, he generado dos nuevos datos que son los beneficios y el retorno de la inversión o ROI.
 
-##### 4. Exploración
+#### 5. Exploración
 En esta etapa hice un análisis univariante, bivariante y multivariante de los datos de valoraciones y recaudación.
 
-##### 5. Conclusiones
+#### 6. Conclusiones
 En el análisis podemos concluir que las valoraciones de los usuarios tienen una mayor tendencia a aprobar las películas, sin embargo las puntuaciones de la crítica son más dispersas e incluso llegan a otorgar la puntuación máxima de 100.
 '''
 
