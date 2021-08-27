ff7ece93f0018567b0f8c6513f54f049d62084a0
Ana Blanco <anablancodelgado@gmail.com>
2021-08-27 20:21:07 +0200

    Update size n_movies_filtered data page streamlit

--- src/utils/streamlit/EDA_IMDb_functions.py
+++ src/utils/streamlit/EDA_IMDb_functions.py
@@ -57,7 +57,7 @@ def bars_nmovies(movies):
     nmovies = movies.groupby('year')['year'].count()
 
 
-    fig, ax = plt.subplots(figsize=(8,4))
+    fig, ax = plt.subplots(figsize=(7.6,3.8))
 
     ax.bar(nmovies.index.astype(int), nmovies.values, color = '#f5c518', edgecolor = "none")
 
