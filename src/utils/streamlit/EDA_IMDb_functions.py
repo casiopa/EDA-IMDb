bfb2ea2a4a86dabd0378cff66091e3d32340c725
Ana Blanco <anablancodelgado@gmail.com>
2021-08-27 20:21:47 +0200

    Update text intro page variables streamlit

--- src/utils/streamlit/EDA_IMDb_functions.py
+++ src/utils/streamlit/EDA_IMDb_functions.py
@@ -57,7 +57,7 @@ def bars_nmovies(movies):
     nmovies = movies.groupby('year')['year'].count()
 
 
-    fig, ax = plt.subplots(figsize=(7.6,3.8))
+    fig, ax = plt.subplots(figsize=(7,3.5))
 
     ax.bar(nmovies.index.astype(int), nmovies.values, color = '#f5c518', edgecolor = "none")
 
