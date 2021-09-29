d9947cd78e930f0008d28ca88f4cf5646e730d88
Ana Blanco <anablancodelgado@gmail.com>
2021-09-29 20:04:32 +0200

    Update EDA_IMDb_variables.py

--- src/utils/streamlit/EDA_IMDb_variables.py
+++ src/utils/streamlit/EDA_IMDb_variables.py
@@ -8,12 +8,12 @@ El objetivo de este estudio es buscar relaciones entre las valoraciones de usuar
 Con la intención de analizar datos representativos de todo tipo de películas se ha acudido al portal IMDb, el más completo a nivel internacional, y se han extraído los datos por dos vías: la descarga de tablas relacionales gratuitas de IMDb y mediante *web scrapping* de las películas del portal IMDb. El alcance de este estudio está limitado por la información accesible de manera gratuita en el portal IMDb, y a nivel temporal, se centra en los años comprendidos entre 2014 y 2019.
 El objetivo inicial era estudiar los 10 años anteriores a 2020 (de 2010 a 2019) pero por falta de recursos solo ha sido posible recoger datos para 6 años (2014 a 2019).
 
-### Principales procesos
+### Procesos
 
 #### 1. Tablas relacionales descargadas de IMBd
 IMDb ofrece, de manera gratuita, una serie de archivos `.csv` que se corresponden con parte la información de su base de datos. De estas tablas he conseguido información como identificador de IMDb, título en español y en su versión original, duración, géneros, rating IMDb, año.
 
-#### 2. *Web scrapping* portal IMBd
+#### 2. *Web scrapping* portal IMDb
 Como los archivos proporcionados por IMDb no contienen información económica de las películas he recogido esta información del propio portal IMDb mediante *web scrapping*. También era importante el dato de valoración de la crítica, el Metascore que sin ser un dato propio de IMBd sí que se puede visualizar en el portal. Además he recogido información que podría ser relevante más adelante como directores, guionistas, actores y países. Debido a la ingente cantidad de información, he tenido que utilizar parallel para recoger información simultáneamente de varias páginas simultáneamente, una página por núcleo del procesador de mi portátil. La herramienta principal en esta estapa fue Selenium.
 > **117.482 páginas escrapeadas** (todas las películas de 2014 a 2020)
 
@@ -28,7 +28,10 @@ Finalmente, teniendo la información de presupuesto y recaudación, he generado 
 
 #### 5. Exploración
 En esta etapa hice un análisis univariante, bivariante y multivariante de los datos de valoraciones y recaudación.
+'''
 
+
+'''
 #### 6. Conclusiones
 En el análisis podemos concluir que las valoraciones de los usuarios tienen una mayor tendencia a aprobar las películas, sin embargo las puntuaciones de la crítica son más dispersas e incluso llegan a otorgar la puntuación máxima de 100.
 '''
